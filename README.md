8df9c7503c3861f411809114dfeef2051a3970db
komodothon <kadir.v@gmail.com>
2024-11-19 15:46:52 +0530

    Flask-SQLAlchemy complex

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Flask-SQLAlchem implementation within a flask web app and managing a SQLite data
 
 Postman VSCode extension used to test the APIs.
 
+In this version, apart from the basics, the functionality involves association of three elements in a one-to-many (Users to Posts), many-to-many (Posts to Tags) relationship.
+
+A multi-model, relational database with features to manage users, their posts, and a tagging system. Providing API to interect with the data, allowing create and read operations (not update/delete) on users, posts and tags.
+
 ---
 
 ## Features and Components
@@ -16,10 +20,29 @@ Postman VSCode extension used to test the APIs.
 
 ### 2. **Database Setup**
 - **SQLAlchemy** is initialized with `Flask` app instance.
-- `User` model defined for a user table in the database with the following columns:
+- `User` model defined for a users table in the database with the following columns:
   - `id`: Primary key, integer.
   - `username`: String, unique, non-nullable.
   - `email`: String, unique, non-nullable.
+  -  `relationship`: with `Post` model with `User` as author. One-To-Many relationship with post. => One user can author many different posts.
+
+- `Post` model defined for posts table in the database with the following columns:
+  - `id`: Primary key, integer.
+  - `title`: String, unique, non-nullable.
+  - `content`: Text, unique, non-nullable.
+  -  `relationship`: with `User` model with `Post` as a post. One post can be authored only one `User`.
+  -  `relationship`: with `Tag` model with `Tag` as a tag. One post can have many tags. Association is secondary as described by the Association Table.
+
+- `Tag` model defined for a tags table in the database with the following columns:
+  - `id`: Primary key, integer.
+  - `name`: String, unique, non-nullable.
+  -  `relationship`: with `Post` model. One `Tag` can be applied to many different `Post` elements. Similarly, one `Post` can have many different `Tag`s
+
+- Association table `post_tags` added in the database with the following columns:
+  - `post_id`: Primary key, integer, is a ForeignKey from ('posts.id').
+  - `tag_id`: Primary key, integer, is a ForeignKey from ('tags.id').
+  - Each association of a tag to a post is a separate row in this table.
+
 - Database tables are created using `db.create_all()` within the application context.
 
 ---
@@ -36,6 +59,27 @@ Postman VSCode extension used to test the APIs.
 #### - **Get All Users (`/users`, GET)**
   - Retrieves all users from the database.
   - Returns a JSON response containing a list of all users.
+
+#### - **Add Post (`/posts`, POST)**
+  - Accepts JSON data to add a new post with `title`, `content` and `author`.
+  - Inserts the new post into the database and commits the transaction.
+
+#### - **Get All Posts (`/posts`, GET)**
+  - Retrieves all posts from the database.
+  - Returns a JSON response containing a list of all posts.
+
+#### - **Add Tag (`/tags`, POST)**
+  - Accepts JSON data to add a new tag with `name`.
+  - Inserts the new tag into the database and commits the transaction.
+
+#### - **Get All Tags (`/tags`, GET)**
+  - Retrieves all tags from the database.
+  - Returns a JSON response containing a list of all tags.
+
+#### - **Add Tag to Post(`/posts/<int:post_id>/tags`, POST)**
+  - Accepts JSON data to add a existing tag to an existing post.
+  - Checks for specific tag and post to exist in the database before association.
+  - Inserts the tag and associated post info into the post_tags table of database and commits the transaction.
 
 ---
 
--- app.py
+++ app.py
@@ -1,32 +1,73 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 # Config SQLA database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///complex_demo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Association table
+post_tags = db.Table(
+    'post_tags', 
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+)
+
+
 # define model
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
+    posts = db.relationship('Post', back_populates='author', cascade='all, delete')
+
     def __repr__(self):
         return f"<User (id:{self.id}, username:{self.username}, email:{self.email})>"
     
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    author = db.relationship('User', back_populates='posts')
+    tags = db.relationship('Tag', secondary=post_tags, back_populates='posts')
+
+    def __repr__(self):
+        return f"<Post(id={self.id}, title={self.title})>"
+    
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+    posts = db.relationship('Post', secondary=post_tags, back_populates='tags')
+
+    def __repr__(self):
+        return f"<Tag(id={self.id}, name={self.name})"
+
+
 # create database tables
 with app.app_context():
     db.create_all()
-    print(app.instance_path)
 
 # routes 
 @app.route("/")
 def home():
-    return "Home page for Flask-SQLAlchemy demo"
+    return "Flask-SQLAlchemy slightly complex demo"
+
+### User routes
 
 @app.route("/users", methods=["POST"])
 def add_user():
@@ -35,6 +76,9 @@ def add_user():
     data = request.get_json()
     username = data.get('username')
     email = data.get('email')
+
+    if not username or not email:
+        return jsonify({'error': 'Username or email is missing'}), 400
 
     if User.query.filter_by(username=username).first():
         return jsonify({'message': 'User already exists!'}), 400
@@ -49,8 +93,97 @@ def add_user():
 def get_users():
     """Retrieve all users."""
     users = User.query.all()
+    print(users)
+    print(f'datatype of users: {type(users)}')
     user_list = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
     return jsonify({'users': user_list})
+
+
+### Posts routes
+
+@app.route("/posts", methods=["POST"])
+def add_post():
+    """Creates a new post"""
+
+    data = request.get_json()
+    title = data.get('title')
+    content = data.get('content')
+    user_id = data.get('user_id')
+
+    if not title or not content or not user_id:
+        return jsonify({'error': 'Invalid/missing credentials'}), 400
+    
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'error': 'user not found'}), 404
+    
+    new_post = Post(title=title, content=content, author=user)
+    db.session.add(new_post)
+    db.session.commit()
+
+    return jsonify({'message': 'Post created', 'post': {'title': title, 'content': content}})
+    
+@app.route("/posts", methods = ["GET"])
+def get_posts():
+    """Get all posts."""
+
+    posts = Post.query.all()
+    post_list = [{'id': post.id, 'title': post.title, 'author': post.author.username} for post in posts]
+    return jsonify({'posts': post_list})
+
+
+### Tag routes
+@app.route("/tags", methods=["POST"])
+def add_tag():
+    """Create new tag"""
+    tag_data = request.get_json()
+    name = tag_data.get('name')
+
+    if not name:
+        return jsonify({'error': 'missing tag name'}), 400
+    
+    if Tag.query.filter_by(name=name).first():
+        return jsonify({'error': 'Tag name already exists'}), 400
+    
+    new_tag = Tag(name=name)
+    db.session.add(new_tag)
+    db.session.commit()
+
+    return jsonify({'message': 'New tag added', 'tag': {'name':name}})
+
+@app.route("/tags", methods=["GET"])
+def get_tags():
+    tags = Tag.query.all()
+    tag_list = [{'id': tag.id, 'name': tag.name} for tag in tags]
+    return jsonify({'tags': tag_list})
+
+
+### Add tags to posts
+@app.route("/posts/<int:post_id>/tags", methods=["POST"])
+def add_tag_to_post(post_id):
+    """Add (a) tag(s) to post."""
+    data = request.get_json()
+    tag_name = data.get('tag')
+
+    if not tag_name:
+        return jsonify({'error': 'No tag name'}), 400
+    
+    post = Post.query.get(post_id)
+    if not post:
+        return jsonify({'error': 'Post not found'}), 404
+    
+    tag = Tag.query.filter_by(name=tag_name).first()
+    if not tag:
+        return jsonify({'error': 'Tag not found'}), 404
+    
+    post.tags.append(tag)
+    db.session.commit()
+
+    return jsonify({'message': 'Tag added to post',
+                    'post': post.title,
+                    'tag': tag.name})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
