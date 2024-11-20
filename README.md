2c20dbe41a9236301a9826d2c85abd2813584e84
komodothon <kadir.v@gmail.com>
2024-11-20 19:33:45 +0530

    Flask-Login implementation

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # flask-sqlalchemy
 Flask-SQLAlchem implementation within a flask web app and managing a SQLite database
 
-Postman VSCode extension used to test the APIs.
+Postman application used to test the APIs routes and endpoints.
 
-In this version, apart from the basics, the functionality involves association of three models in a one-to-many (Users to Posts), many-to-many (Posts to likes) relationship.
+In this version, apart from the basics, the functionality involves association of two models (User and Post).
 
-A multi-model, relational database with features to manage users, their posts, and commenting and likes system. Providing API to interect with the data, allowing create and read operations (not update/delete) on users, posts and tags.
+A multi-model, relational database with features to manage users and their posts. Providing API to interect with the data, allowing create and read operations (not update/delete) on users, posts and tags.
 
-Session management is implemented using session object from Flask.
+~~Session management is implemented using session object from Flask.~~
+
+Flask-Login library and it's elements are implemented for user management, user authentication and user session management.
 
 ---
 
@@ -24,27 +26,25 @@ Session management is implemented using session object from Flask.
   - `id`: Primary key, integer.
   - `username`: String, unique, non-nullable.
   - `email`: String, unique, non-nullable.
-  -  `relationship`: with `Post` model with `User` as author. One-To-Many relationship with post. => One user can author many different posts.
+  -  `relationship`: with `Post` model with `User` as author. One-To-Many relationship with post. One user can author many different posts.
 
 - `Post` model defined for posts table in the database with the following columns:
   - `id`: Primary key, integer.
   - `title`: String, unique, non-nullable.
   - `content`: Text, unique, non-nullable.
   -  `relationship`: with `User` model with `Post` as a post. One post can be authored only one `User`.
-  -  `relationship`: with `Tag` model with `Tag` as a tag. One post can have many tags. Association is secondary as described by the Association Table.
+ -  ~~ `relationship`: with `Tag` model with `Tag` as a tag. One post can have many tags. Association is secondary as described by the Association Table.~~
 
-- `Comment` model defined for a comments table in the database with the following columns:
-  - `id`: Primary key, integer.
-  - `content`: String, unique, non-nullable.
-  -  `user_id`: id of the commenting user as foreign key.
-    - `post_id`: related post for this comment, as foreign key.
+- ~~`Comment` model defined for a comments table in the database with the following columns:~~
+ ~~ - `id`: Primary key, integer.~~
+~~  - `content`: String, unique, non-nullable.~~
+~~  -  `user_id`: id of the commenting user as foreign key.~~
+~~    - `post_id`: related post for this comment, as foreign key.~~
 
-- Association table `likes` added in the database with the following columns:
-  -  `user_id`: id of the commenting user as foreign key.
-    - `post_id`: related post for this comment, as foreign key.
-
-- Database tables are created using `db.create_all()` within the application context.
-
+- ~~Association table `likes` added in the database with the following columns:~~
+~~  -  `user_id`: id of the commenting user as foreign key.~~
+~~    - `post_id`: related post for this comment, as foreign key.~~
+~~- Database tables are created using `db.create_all()` within the application context.~~
 ---
 
 ### 3. **Routes**
@@ -61,21 +61,16 @@ Session management is implemented using session object from Flask.
   - Logs a user in.
   - initiates a session for that user.
 
+#### - **Logout (`/logout`, POST)**
+  - Logs out the current user.
+  - closes the session of current user.
+
 #### - **Create Post (`/posts`, POST)**
   - Accepts JSON data to add a new post with `title`, `content` and utilises user detail from `session` to be author of the post.
   - Inserts the new post into the database and commits the transaction.
 
-#### - **like a post (`/posts/<int:post_id>/like`, POST)**
-  - Accepts JSON data to like a post.
-  - Inserts the new like into the database association table and commits the transaction.
-
-#### - **Add comment (`/posts/<int:post_id>/comments`, POST)**
-  - Accepts JSON data to add a new comment with `content`.
-  - retrieves detail of commenting user from `session`.
-  - Inserts the new comment into the database and commits the transaction.
-
 #### - **Get All Posts (`/posts`, GET)**
-  - Retrieves all posts from the database.
+  - Retrieves all posts from the database by current user.
   - Returns a JSON response containing a list of all posts.
 
 ---
--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
-
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
-
 
 # Configurations
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
@@ -16,6 +14,12 @@ app.config['SECRET_KEY'] = 'app_secret_key'
 # Extensions
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+# Flask-Login setup
+login_manager.login_view = 'login'
+login_manager.login_message = 'Please login to access this page.'
+login_manager.login_message_category = 'info'
 
 # Many-To-Many: Users like Postss (Association table)
 likes = db.Table(
@@ -26,7 +30,7 @@ likes = db.Table(
 
 
 # define models
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -35,8 +39,8 @@ class User(db.Model):
     password = db.Column(db.String(128), nullable=False)
 
     posts = db.relationship('Post', back_populates='author', cascade='all, delete-orphan')
-    comments = db.relationship('Comment', back_populates='author', cascade='all,delete-orphan')
-    liked_posts = db.relationship('Post', secondary=likes, back_populates='likers')
+    # comments = db.relationship('Comment', back_populates='author', cascade='all,delete-orphan')
+    # liked_posts = db.relationship('Post', secondary=likes, back_populates='likers')
 
     def __repr__(self):
         return f"<User (id:{self.id}, username:{self.username}, email:{self.email})>"
@@ -51,36 +55,42 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     author = db.relationship('User', back_populates='posts')
-    comments = db.relationship('Comment', back_populates='post', cascade='all, delete-orphan')
-    likers = db.relationship('User', secondary=likes, back_populates='liked_posts')
+    # comments = db.relationship('Comment', back_populates='post', cascade='all, delete-orphan')
+    # likers = db.relationship('User', secondary=likes, back_populates='liked_posts')
 
     def __repr__(self):
         return f"<Post(id={self.id}, title={self.title}, author={self.user_id})>"
     
 
-class Comment(db.Model):
-    __tablename__ = 'comments'
+# class Comment(db.Model):
+#     __tablename__ = 'comments'
 
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.Text, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
 
-    author = db.relationship('User', back_populates='comments')
-    post = db.relationship('Post', back_populates='comments')
+#     author = db.relationship('User', back_populates='comments')
+#     post = db.relationship('Post', back_populates='comments')
 
-    def __repr__(self):
-        return f"<Comment(id={self.id}, post_id={self.post_id}, user_id={self.user_id})>"
+#     def __repr__(self):
+#         return f"<Comment(id={self.id}, post_id={self.post_id}, user_id={self.user_id})>"
 
 
 # create database tables
 with app.app_context():
     db.create_all()
 
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+
 # routes 
 @app.route("/")
 def home():
-    return "Flask-SQLAlchemy demo. Blog with users, posts and comments"
+    return "Flask-SQLAlchemy demo. Flask-Login implementation."
 
 ### User routes
 
@@ -102,6 +112,7 @@ def register():
 
     return jsonify({'message': 'User registered successfully'}), 201
 
+
 @app.route("/login", methods=["POST"])
 def login():
     """Authenticate an user"""
@@ -114,25 +125,29 @@ def login():
         user = User.query.filter_by(username=username).first()
     
     if user and bcrypt.check_password_hash(user.password, password):
-        session['user_id'] = user.id
-        session['username'] = user.username
-        return jsonify({'message': f'Login successful for {session['username']}'}), 200
+        login_user(user)
+        return jsonify({'message': f'Login successful for {user.username}'}), 200
 
     return jsonify({'message': 'Invalid credentials'}), 401
 
 
+@app.route("/logout", methods=["POST"])
+def logout():
+    """Logout the user by clearing session."""
+    logout_user()
+    return jsonify({'message': 'Logged out'}), 200
+
+
 ### Post routes
-@app.route("/posts", methods=["POST"])
+@app.route("/create_post", methods=["POST"])
+@login_required
 def create_post():
     """Create a new post."""
-    if 'user_id' not in session:
-        return jsonify({'message': 'Login required'}), 401
 
     data = request.get_json()
     title = data.get('title')
     content = data.get('content')
-
-    user_id = session['user_id']
+    user_id = current_user.id
 
     new_post = Post(title=title, content=content, user_id=user_id)
     db.session.add(new_post)
@@ -140,80 +155,15 @@ def create_post():
 
     return jsonify({'message': 'Post created successfully', 'post': {'title': title, 'content': content}}), 201
 
-@app.route("/posts/<int:post_id>/like", methods=["POST"])
-def like_post(post_id):
-    """Like or unlike a post."""
+@app.route("/my_posts", methods=["GET"])
+@login_required
+def get_my_posts():
+    """List all posts by the current user"""
 
-    if 'user_id' not in session:
-        return jsonify({'message': 'Login required'}), 401
+    posts = Post.query.filter_by(user_id=current_user.id).all()
+    post_list = [{'id': post.id, 'title': post.title, 'content': post.content}for post in posts]
 
-    user_id = session['user_id']
-
-    user = User.query.get(user_id)
-    post = Post.query.get(post_id)
-
-    if not post:
-        return jsonify({'message': "Post not found"}), 404
-    
-    if post in user.liked_posts:
-        user.liked_posts.remove(post)
-        action = 'unliked'
-    else:
-        user.liked_posts.append(post)
-        action = 'liked'
-
-    db.session.commit()
-    return jsonify({'message': f'Post {action} successfully'})
-
-
-@app.route("/posts/<int:post_id>/comments", methods=["POST"])
-def add_comment(post_id):
-    """Add a comment to a post."""
-
-    if 'user_id' not in session:
-        return jsonify({'message': 'Login required'}), 401
-    
-    data = request.get_json()
-    content = data.get('content')
-    
-    user_id = session['user_id']
-
-    post = Post.query.get(post_id)
-
-    if not post:
-        return jsonify({'message': 'Post not found'}), 404
-    
-    new_comment = Comment(content=content, user_id=user_id, post_id=post_id)
-    db.session.add(new_comment)
-    db.session.commit()
-
-    return jsonify({'message': 'Comment added successfully'}), 201
-
-
-@app.route("/posts", methods=["GET"])
-def get_posts():
-    """List of all posts with comments and likes."""
-    posts = Post.query.all()
-    post_list = []
-
-    for post in posts:
-        post_info = {
-            'id': post.id,
-            'title' : post.title,
-            'content' : post.content,
-            'author' : post.author.username,
-            'likes' : len(post.likers),
-            'comments' : [{'content': comment.content, 'author': comment.author.username} for comment in post.comments]
-        }
-        post_list.append(post_info)
-    
     return jsonify({'posts': post_list})
-
-@app.route("/logout", methods=["POST"])
-def logout():
-    """Logout the user by clearing session."""
-    session.clear()
-    return jsonify({'message': 'Logged out'}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
